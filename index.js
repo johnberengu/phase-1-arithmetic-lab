9615e6212c7c4ec6ebdf94cecdd160ac33402c7a
eli <john.berengu@student.moringaschool.com>
2024-11-29 15:35:54 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+// 1
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
+console.log(multiply)
+// 2
+function intager() {
+    return Math.floor(Math.random()) + 1; 
+}
+const random = intager();
+// 3 
+const num3 = 8
+const num4 = 2
+const mod = num3/num4;
+console.log(mod);
+// 4
+const num = [5, 6, 19, 20];
+const max = Math.max(...num);
+console.log(max);
+
+
+
 
 // Write your code here
